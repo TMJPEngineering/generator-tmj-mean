36cc694ddd90deae9079b22acb8599c1f27d60d9
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-07 16:37:35 +0800

    update version and router done refactor

--- generators/app/templates/vendor/router.js
+++ generators/app/templates/vendor/router.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var ctrl = require('./controller'),
-    middleware = require('./middleware');
+    middleware = require('./middleware'),
+    router;
 
 module.exports = {
     config: {
         app: null,
+        set: function (controller) {
+            router = {
+                str: controller.split('@'),
+                controller: controller.split('@').shift(),
+                module: controller.split('@').shift().split('.')
+            };
+        },
         callback: function (res, req, next) {
             next();
         }
@@ -14,29 +22,21 @@ module.exports = {
         this.config.app = app;
     },
     get: function (uri, controller, auth) {
-        var str = controller.split('@'),
-            controller = str.shift(),
-            module = controller.split('.');
-        this.config.app.get(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(controller, str.shift(), module.shift()));
+        this.config.set(controller);
+        this.config.app.get(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(router.controller, router.str.pop(), router.module.shift()));
         return this;
     },
     post: function (uri, controller, auth) {
-        var str = controller.split('@'),
-            controller = str.shift(),
-            module = controller.split('.');
-        this.config.app.post(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(controller, str.shift(), module.shift()));
+        this.config.set(controller);
+        this.config.app.post(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(router.controller, router.str.pop(), router.module.shift()));
     },
     update: function (uri, controller, auth) {
-        var str = controller.split('@'),
-            controller = str.shift(),
-            module = controller.split('.');
-        this.config.app.put(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(controller, str.shift(), module.shift()));
+        this.config.set(controller);
+        this.config.app.put(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(router.controller, router.str.pop(), router.module.shift()));
     },
     delete: function (uri, controller, auth) {
-        var str = controller.split('@'),
-            controller = str.shift(),
-            module = controller.split('.');
-        this.config.app.delete(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(controller, str.shift(), module.shift()));
+        this.config.set(controller);
+        this.config.app.delete(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(router.controller, router.str.pop(), router.module.shift()));
     },
     all: function (uri, callback) {
         this.config.app.all(uri, function (req, res) {
