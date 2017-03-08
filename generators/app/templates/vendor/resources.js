a748f3454eece4fb2303e6cd73324c12b6ae270b
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-08 13:29:19 +0800

    add resource method

---
+++ generators/app/templates/vendor/resources.js
@@ -0,0 +1,3 @@
+'use strict';
+
+module.exports = ['index', 'create', 'show', 'edit', 'store', 'update', 'destroy'];
--- generators/app/templates/vendor/router.js
+++ generators/app/templates/vendor/router.js
@@ -2,6 +2,7 @@
 
 var ctrl = require('./controller'),
     middleware = require('./middleware'),
+    resources = require('./resources'),
     router;
 
 module.exports = {
@@ -37,6 +38,41 @@ module.exports = {
     delete: function (uri, controller, auth) {
         this.config.set(controller);
         this.config.app.delete(uri, (auth) ? middleware(auth) : this.config.callback, ctrl(router.controller, router.str.pop(), router.module.shift()));
+    },
+    resource: function (uri, controller, auth, options) {
+        this.config.set(controller);
+        var $this = this;
+        resources = resources.filter(function(value) {
+            if (options !== undefined && options.only !== undefined)
+                return options.only.indexOf(value) !== -1;
+            if (options !== undefined && options.except !== undefined)
+                return options.except.indexOf(value) !== 0;
+            return true;
+        });
+        resources.forEach(function (resource) {
+            if (resource != 'store' && resource != 'update' && resource != 'delete') {
+                var str;
+                if (resource == 'create') {
+                    str = '/create';
+                }
+                if (resource == 'show') {
+                    str = '/:id';
+                }
+                if (resource == 'edit') {
+                    str = '/:id/edit';
+                }
+                $this.get(uri + str, router.controller + '@' + resource, auth);
+            }
+            if (resource == 'store') {
+                $this.post(uri, router.controller + '@' + resource, auth);
+            }
+            if (resource == 'update') {
+                $this.put(uri + '/:id', router.controller + '@' + resource, auth);
+            }
+            if (resource == 'delete') {
+                $this.delete(uri + '/:id', router.controller + '@' + resource, auth);
+            }
+        });
     },
     all: function (uri, callback) {
         this.config.app.all(uri, function (req, res) {
