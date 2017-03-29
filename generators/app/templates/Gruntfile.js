bcbe58d5feea800c06b9a6e962aaba2e725dabcf
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-29 09:43:42 +0800

    fix dev and prod for grunt

--- generators/app/templates/Gruntfile.js
+++ generators/app/templates/Gruntfile.js
@@ -104,5 +104,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('default', ['browserify', 'uglify', 'sass', 'copy', 'watch']);
+    grunt.registerTask('default', ['browserify', 'sass', 'copy', 'watch']);
+    grunt.registerTask('prod', ['browserify', 'uglify', 'sass', 'copy', 'watch']);
 };
